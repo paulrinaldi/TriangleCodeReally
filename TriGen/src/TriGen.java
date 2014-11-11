a5ff93e2acdbda8d2b38489faf486a4c5a9fbfeb
Becky Daniels <book.music.wordlover@gmail.com>
2014-11-10 18:54:54 -0600

    Triangle Code
    Nick's code for the triangle (not Pascal's)

---
+++ TriGen/src/TriGen.java
@@ -0,0 +1,37 @@
+public class TriGen {
+
+	public static void main(String[] args) {
+
+		if (genTri(10000) == 1) {
+			System.out.println("Cannot handle input. Error: too large");
+		}
+	}
+
+	private static int genTri(int size) {
+
+		if (size > 1000) {
+			return 1;
+		}
+
+		for (int a = 1; a <= size; a++) {
+			for (int dummy = 1; dummy <= a; dummy++) {
+
+				System.out.print(dummy + " ");
+			}
+
+			System.out.print("\n");
+		}
+
+		for (int b = size - 1; b >= 0; b--) {
+			for (int dummy = b; dummy >= 1; dummy--) {
+
+				System.out.print(dummy + " ");
+			}
+
+			System.out.print("\n");
+		}
+
+		return 0;
+	}
+
+}
